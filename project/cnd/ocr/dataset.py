c0c21131bdd396ede894cf05a7dab5f3dbde5e75
Egor Dudyrev <egor.dudyrev@yandex.ru>
2020-02-29 18:22:41 +0300

    Add transforms and dataset

--- project/cnd/ocr/dataset.py
+++ project/cnd/ocr/dataset.py
@@ -1,12 +1,18 @@
-import cv2
+import os
+import re
 import numpy as np
+import cv2
 
-from torch.utils.data import Dataset
+#from torch.utils.data import Dataset
 
 
-class OcrDataset(Dataset):
+class OcrDataset():#Dataset):
     def __init__(self, data_path, target_path, transforms=None):
         # TODO: Here you can create samples from dirs and initialize transfroms
+        def load(x):
+            fnames = sorted([os.path.join(data_path,fname) for fname in os.listdir(data_path) if
+                             re.fullmatch('[a-zA-Z]\d{3}[a-zA-Z]{2} \d+\.bmp', fname) is not None])
+            return fnames
         self.data = load(data_path)
         self.target = load(target_path)
         self.transforms = transforms
@@ -16,8 +22,10 @@ class OcrDataset(Dataset):
 
     def __getitem__(self, idx):
         img = cv2.imread(self.data[idx])
-        t = np.load(self.target[idx])
+        t = os.path.split(self.target[idx])[-1][:5] #np.load(self.target[idx])#
         # TODO: Apply transforms to img and target if it necessary
+        for trns in self.transforms:
+            img = trns(img)
 
         return img, t
 
--- project/cnd/ocr/transforms.py
+++ project/cnd/ocr/transforms.py
@@ -1,5 +1,7 @@
-import torch
+#import torch
 import numpy as np
+import skimage
+import cv2
 
 
 class ImageNormalization(object):
@@ -7,10 +9,91 @@ class ImageNormalization(object):
         return image / 255.
 
 
-class ToTensor(object):
-    def __call__(self, image):
-        image = image.astype(np.float32)
-        return torch.from_numpy(image)
+class ImageScaling(object):
+    def __init__(self, dsize):
+        self._dsize = dsize
+
+    def __call__(self, image, dsize=None):
+        dsize = self._dsize if dsize is None else dsize
+        return cv2.resize(image, (dsize[1], dsize[0]))
+
+class ToType(object):
+    def __init__(self, type_=np.float32):
+        self._type_ = type_
+
+    def __call__(self, image, type_=None):
+        type_ = self._type_ if type_ is None else type_
+        return image.astype(type_)
+
+
+class ToGreyScale(object):
+    def __call__(self, image, strategy='sklearn'):
+        gscale = image
+        if strategy=='sklearn':
+            # Y = 0.2125 R + 0.7154 G + 0.0721 B. These weights represent human colour perception better than just mean values. According to sklean
+            gscale[:, :, 0] = gscale.dot([0.2125, 0.7154, 0.0721])
+        elif strategy=='mean':
+            gscale[:, :, 0] = gscale.dot([1, 1, 1])/3
+        gscale[:, :, 1] = gscale[:, :, 0]
+        gscale[:, :, 2] = gscale[:, :, 0]
+        return gscale
+
+
+class RandomCrop(object):
+    def __init__(self, size):
+        self._size = size
+
+    def __call__(self, img, size=None, seed=None):
+        size = self._size if size is None else size
+        assert size[0] < img.shape[0], 'Size height is bigger than image'
+        assert size[1] < img.shape[1], 'Size width is bigger than image'
+
+        y_max = img.shape[0] - size[0]
+        x_max = img.shape[1] - size[1]
+
+        np.random.seed(seed)
+        x, y = [np.round(np.random.uniform(low=0, high=lim), 0).astype(int) for lim in [x_max, y_max]]
+        return img[y:y + size[0], x:x + size[1]]
+
+
+class ImageRotation(object):
+    def __init__(self, angle='random'):
+        self._angle = angle
+
+    def __call__(self, img, angle=None, seed=None):
+        np.random.seed(seed)
+        angle = self._angle if angle is None else angle
+        if angle=='random':
+            angle = np.random.uniform(-10,10)#+np.random.choice([0,180])
+        return skimage.transform.rotate(img, angle)
+
+
+class ChangeBrightnessContrast(object):
+    def __init__(self, brightness='random', contrast='random'):
+        self._brightness = brightness
+        self._contrast = contrast
+
+    def __call__(self, img, brightness=None, contrast=None, seed=None):
+        if img.max()<=1:
+            img = img*255
+
+        brightness = self._brightness if brightness is None else brightness
+        contrast = self._contrast if contrast is None else contrast
+
+        np.random.seed(seed)
+        if brightness=='random':
+            brightness = np.round(np.random.uniform(-140, 140), 0).astype(int)
+        if contrast=='random':
+            contrast = np.random.uniform(0.3,2)
+
+        new_img = ImageNormalization()(cv2.convertScaleAbs(img, alpha=contrast, beta=brightness))
+        return new_img
+
+
+#class ToTensor(object):
+#    def __call__(self, image):
+#        image = image.astype(np.float32)
+#        return torch.from_numpy(image)
 
 #TODO: Your transforms here
 # Basic transforms:
